cd692587715b74cad5d47cf57861353cb809dce6
Michael Carroll <128497239+mcarroll138@users.noreply.github.com>
2023-08-04 14:02:33 -0700

    create migration for SQL tables

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -7,6 +7,6 @@ namespace Factory.Models
         public int EngineerId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public List<Machine> Machines { get; set; }
+        public List<EngineerMachine> JoinEntities { get; }
     }
 }
