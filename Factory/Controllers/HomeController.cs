825d1cb51d828c05b3f469f296118d9e7e0935a7
Michael Carroll <128497239+mcarroll138@users.noreply.github.com>
2023-08-04 14:15:32 -0700

    update HomeController.cs

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,14 +1,25 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly FacotryContext _db;
+    public HomeController(FactoryContext db)
+    {
+        _db = db;
+    }
 
     public ActionResult Index()
     {
+        List<Engineer> engineers = _db.Engineers.ToList();
+        List<Machine> machines = _db.Machines.ToList()
+
+
         return View();
     }
 }
