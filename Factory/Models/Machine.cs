c499393525241fabb798ff7bad87b680ca2575d3
Michael Carroll <128497239+mcarroll138@users.noreply.github.com>
2023-08-04 13:33:10 -0700

    debug, update models

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -7,7 +7,7 @@ namespace Factory.Models
     public int MachineId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public List<Engineer> Engineers { get; set; }
   }
 }
 
-// public List<Engineer> Engineers { get; set; }
